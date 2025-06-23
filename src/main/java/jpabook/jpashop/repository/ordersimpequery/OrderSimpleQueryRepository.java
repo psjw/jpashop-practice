28d58f44284259085e9ec09fce1bcd0ce0b573fc
psjw <dlrudtn108@gmail.com>
2025-06-23 17:22:30 +0900

    주문 조회 개발

--- src/main/java/jpabook/jpashop/repository/ordersimpequery/OrderSimpleQueryRepository.java
+++ src/main/java/jpabook/jpashop/repository/ordersimpequery/OrderSimpleQueryRepository.java
@@ -12,7 +12,7 @@ public class OrderSimpleQueryRepository {
     private final EntityManager em;
 
     public List<OrderSimpleQueryDto> findOrderDtos() {
-        return em.createQuery("select new jpabook.jpashop.repository.OrderSimpleQueryDto(o.id, m.name, o.orderDate, o.status, d.address) " +
+        return em.createQuery("select new jpabook.jpashop.repository.ordersimpequery.OrderSimpleQueryDto(o.id, m.name, o.orderDate, o.status, d.address) " +
                         "from Order o " +
                         "join o.member m " +
                         "join o.delivery d", OrderSimpleQueryDto.class)
